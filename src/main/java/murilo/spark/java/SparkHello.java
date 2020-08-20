22e156639de90b888223f2717f76495c101a5a91
Murilo Henrique Gomes <t1112crl@prestadorcbmp.com.br>
2020-08-20 16:57:49 -0300

    lendo arquivo com dataset

--- src/main/java/murilo/spark/java/SparkHello.java
+++ src/main/java/murilo/spark/java/SparkHello.java
@@ -2,8 +2,20 @@ package murilo.spark.java;
 
 import static spark.Spark.*;
 
+import org.apache.spark.api.java.function.FilterFunction;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.SparkSession;
+
 public class SparkHello {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+
+        SparkSession spark =  SparkSession.builder()
+                .master("local")
+                .appName("Word Count")
+                .getOrCreate();
+        Dataset<String> logData = spark.read().textFile( "/home/murilohg/Documentos/helloworld.txt" );
+        long numAs = logData.filter((FilterFunction<String>) s -> s.contains("a")).count();
+        logData.filter("joao").show();
+
     }
 }
